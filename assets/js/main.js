883186da2488118dfa0f68790030249b85fa9485
zelsec <davcoz69@gmail.com>
2022-05-28 12:18:37 +0200

    updated

---
+++ assets/js/main.js
@@ -0,0 +1,7 @@
+/* COPY FUnction for contact.html */
+
+function copyText() {
+    var popup = document.getElementById("myPopup");
+    navigator.clipboard.writeText("EbicVlad 冰淇淋#5059");
+    popup.classList.toggle("show");
+}
